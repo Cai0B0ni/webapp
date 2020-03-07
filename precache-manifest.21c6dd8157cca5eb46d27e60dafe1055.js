e29efcf236c2ab0c85dfe29ab74dae331484a27f
caio <caio@omicmd.com>
2020-03-06 16:19:46 -0800

    Updates

---
+++ precache-manifest.21c6dd8157cca5eb46d27e60dafe1055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc676fdc9e707d6e58a2a3a309fa5e4",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "0f23dc1306d422d4205a",
+    "url": "/webapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7cc2cc16f34c2a4ba55d",
+    "url": "/webapp/static/js/2.d08ed514.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/webapp/static/js/2.d08ed514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f23dc1306d422d4205a",
+    "url": "/webapp/static/js/main.91357fc6.chunk.js"
+  },
+  {
+    "revision": "67a82fa829b914f723b3",
+    "url": "/webapp/static/js/runtime-main.6c87fd0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.016e6c18fc4f6cd02a4cd08dce04c46e.js"
+  "/webapp/precache-manifest.21c6dd8157cca5eb46d27e60dafe1055.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
