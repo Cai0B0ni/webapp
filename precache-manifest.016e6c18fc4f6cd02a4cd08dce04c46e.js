57e7d59233ee702df0d059f2d54b027db2618476
caio <caio@omicmd.com>
2020-03-06 16:07:33 -0800

    Updates

---
+++ precache-manifest.016e6c18fc4f6cd02a4cd08dce04c46e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c457da9846e7a970633834a144c586",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "68f4d3894259e6be5061",
+    "url": "/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7cc2cc16f34c2a4ba55d",
+    "url": "/website/static/js/2.d08ed514.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/website/static/js/2.d08ed514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f4d3894259e6be5061",
+    "url": "/website/static/js/main.feeda2de.chunk.js"
+  },
+  {
+    "revision": "02403b8428257a0715b8",
+    "url": "/website/static/js/runtime-main.da877716.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
