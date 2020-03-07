9fcf31a2b71b96e63a835b6eb0f58105dff6aaf9
caio <caio@omicmd.com>
2020-03-06 16:23:17 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit Xiop<code>src/App.js</code> and save to reload.
+        </p>SSS
         <a
           className="App-link"
           href="https://reactjs.org"
